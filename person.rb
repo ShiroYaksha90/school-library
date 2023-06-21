67819294d13c721252bca336b592985a5ecdecbf
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-06-21 18:11:37 +0300

    Implement decorate pattern

--- person.rb
+++ person.rb
@@ -1,4 +1,9 @@
-class Person
+class Nameable
+  def correct_name
+    raise NotImplementedError
+  end
+end
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
@@ -12,9 +17,39 @@ class Person
     @parent_permission || of_age?
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+  input = @nameable.correct_name
+  input.length <= 10 ? input : input[0..9]
+  end
+end
+
+person = Person.new(22, 'maximilianus')
+p person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+p capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+p capitalizedTrimmedPerson.correct_name
